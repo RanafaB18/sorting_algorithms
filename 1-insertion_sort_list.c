ff3c21655063bcc3b2b1b4c850cb1f742c0bf361
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-20 20:14:37 +0000

    Edited 0

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *head = *list;
+    listint_t *prev = *list;
+    listint_t *next = head->next;
+
+    while (head)
+    {
+        if (head->n > next->n)
+        {
+            
+        }
+    }
+}
