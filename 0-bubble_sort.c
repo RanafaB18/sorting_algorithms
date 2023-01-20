5e6d1e0b20607c2a376a81695a9c49e968a776fe
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-20 16:27:38 +0000

    Bettified

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,24 +4,31 @@
  * bubble_sort -  sorts an array of integers in ascending order
  * @array: array
  * @size: size of array
-*/
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
+	size_t i, j;
 
-    for (i = 0; i < size; i++){
-        for (j = 1; j < size - i; j++){
-            if (array[j] < array[j - 1]){
-                swap(&array[j - 1], &array[j]);
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size - i; j++)
+		{
+			if (array[j] < array[j - 1])
+			{
+				swap(&array[j - 1], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
 }
-
+/**
+ * swap -  swaps 2 numbers
+ * @first: first number
+ * @second: second number
+ */
 void swap(int *first, int *second)
 {
-    int temp = *first;
-    *first = *second;
-    *second = temp;
+	int temp = *first;
+	*first = *second;
+	*second = temp;
 }
