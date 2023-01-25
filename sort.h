d45f8dd751acfcbfdbe711553c794993d450f2bd
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-25 01:26:52 +0000

    Counting sort

--- sort.h
+++ sort.h
@@ -24,7 +24,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-
+void counting_sort(int *array, size_t size);
 
 void swap_nodes(listint_t **head, listint_t **n1, listint_t *n2);
 void swap(int *first, int *second);
