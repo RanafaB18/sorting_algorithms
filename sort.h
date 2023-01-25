6d0a036c7fb068a190b48f7acfbb27cc0562248d
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-25 01:46:41 +0000

    Radix sort

--- sort.h
+++ sort.h
@@ -28,6 +28,7 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 void swap_nodes(listint_t **head, listint_t **n1, listint_t *n2);
 void swap(int *first, int *second);
